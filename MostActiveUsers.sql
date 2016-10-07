f2259e919b769d431dd6ea995f6dce691dd888e7
Jake Konoske <jkonoske@doximity.com>
2016-10-07 11:14:16 -0700

    add mobile flag
    Adds a check for whether or not a user is mobile user. 0 if no, 1 if yes

--- MostActiveUsers.sql
+++ MostActiveUsers.sql
@@ -5,6 +5,7 @@ SELECT
 	a.description,
 	f.name,
 	group_concat(distinct d.category) as activities, 
+	CASE WHEN g.num_login_mobile >0  THEN 1 ELSE 0 END as mobile_user,
 	count(d.id) as number_of_events
 FROM
 	3md.users a
@@ -13,6 +14,7 @@ FROM
 	join analytics.dashboard_aggregate d on d.user_id = a.id
 	join 3md.user_professional_details e on e.id = a.`user_professional_details_id`
 	join 3md.specialties f on f.`code` = e.`specialty_id`
+	join analytics.feature_library g on g.user_id = a.id
 WHERE
 	c.workable_id = 782-- facility_id
 	and c.workable_type = 'facility'
