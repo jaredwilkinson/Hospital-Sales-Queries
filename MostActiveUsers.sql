7c4db6a0bf39550d82384adbdeb0ab1fec647174
Jake Konoske <jkonoske@doximity.com>
2016-10-07 09:26:20 -0700

    Create MostActiveUsers.sql
    To find the top 100 most active users at a facility_id

---
+++ MostActiveUsers.sql
@@ -0,0 +1,27 @@
+SELECT 
+	a.id as user_id, 
+	a.firstname,
+	a.lastname,
+	a.description,
+	f.name,
+	group_concat(distinct d.category) as activities, 
+	count(d.id) as number_of_events
+FROM
+	3md.users a
+	join 3md.employment_histories b on b.user_id = a.id
+	join 3md.employment_history_locations c on c.id = b.`employment_history_location_id`
+	join analytics.dashboard_aggregate d on d.user_id = a.id
+	join 3md.user_professional_details e on e.id = a.`user_professional_details_id`
+	join 3md.specialties f on f.`code` = e.`specialty_id`
+WHERE
+	c.workable_id = 782-- facility_id
+	and c.workable_type = 'facility'
+	and d.`day_date` > '2016-09-06'
+	and b.current = 1
+	and platform != 'amion'
+GROUP BY
+	a.id
+ORDER BY
+	count(d.id) DESC
+LIMIT 100
+;
